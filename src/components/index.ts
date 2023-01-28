b1a314e883904329ff710c7e86a05d23e5f2a6ff
Deepak Rawat <awsmyo1234@gmail.com>
2023-01-28 13:15:23 +0530

    bugfix - updated paths

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,7 @@
 export {default as Timeline } from './Timeline'
+export {default as TimelineItem} from './TimelineItem'
+export {default as TimelineItemDate} from './TimelineItemDate'
+export {default as Separator} from './Separator'
+export {default as TimelineItemContent} from './TimelineItemContent'
+export {default as TimelineItemTitle} from './TimelineItemTitle'
+export {default as TimelineItemBody} from './TimelineItemBody'
