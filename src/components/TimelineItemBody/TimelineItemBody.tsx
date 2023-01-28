22346ea48cab643e7742e2982d821919d6f48c47
Deepak Rawat <awsmyo1234@gmail.com>
2023-01-28 15:54:16 +0530

    bugfix

--- src/components/TimelineItemBody/TimelineItemBody.tsx
+++ src/components/TimelineItemBody/TimelineItemBody.tsx
@@ -4,6 +4,7 @@ export interface TimelineItemBodyProps{
   children: ReactNode
 }
 const TimelineItemBody:FC<TimelineItemBodyProps> = ({children}:TimelineItemBodyProps) => {
+
   return (
     <div className="item-content__body">
       {children}
