7deae6e57a10bf700c11827dea6b9b55c9d23700
Deepak Rawat <awsmyo1234@gmail.com>
2023-02-05 08:40:18 +0530

    bugfix

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
+// Timeline
 export {default as Timeline } from './Timeline'
 export {default as TimelineItem} from './TimelineItem'
 export {default as TimelineItemDate} from './TimelineItemDate'
@@ -5,3 +6,6 @@ export {default as Separator} from './Separator'
 export {default as TimelineItemContent} from './TimelineItemContent'
 export {default as TimelineItemTitle} from './TimelineItemTitle'
 export {default as TimelineItemBody} from './TimelineItemBody'
+
+// Modal
+export {default as Modal} from './Modal'
